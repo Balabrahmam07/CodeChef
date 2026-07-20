7f9f5ad956e43efa5a201952cb9676a08ed4873d
Balabrahmam07 <136158779+Balabrahmam07@users.noreply.github.com>
2026-07-20 21:36:33 +0530

    ✅ SQLPBP05 | Medium | SQL | Runtime: N/A | Memory: N/A | 2026-07-20

---
+++ codechef/medium/SQLPBP05/solution.sql
@@ -0,0 +1,11 @@
+SELECT 
+    m.match_id, 
+    m.player_1, 
+    m.player_2, 
+    m.winner, 
+    m.match_date, 
+    p.score
+FROM Players p 
+JOIN Matches m ON p.player_name = m.winner   
+ORDER BY m.match_date DESC 
+LIMIT 5;
